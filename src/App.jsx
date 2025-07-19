fa6652a2da7e3e0e86038810ce2899dd15b4246a
jubayer <jubayerahmed.dev@gmail.com>
2025-07-19 07:39:40 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h2 className="text-7xl font-extrabold bg-gray-600 text-white p-14">
+        Dine Out - ECommerce checkout.
+      </h2>
+    </div>
+  );
+}
+
+export default App;
